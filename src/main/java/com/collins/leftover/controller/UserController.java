b93c4ce5be65fbe236f11b7f6318fd3b805310a4
colllins <lekecollins2@gmail.com>
2026-01-18 17:28:07 -0500

    implemented createUser, retrieveUser controllers on the UserController

---
+++ src/main/java/com/collins/leftover/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.collins.leftover.controller;
+
+import com.collins.leftover.dto.user.AuthResponseDto;
+import com.collins.leftover.dto.user.RegisterRequestDto;
+import com.collins.leftover.service.UserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Validated
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/api/auth/register")
+    public AuthResponseDto createUser(@Valid @RequestBody RegisterRequestDto user){
+        return userService.registerUser(user);
+    }
+
+    @GetMapping("/api/users/{id}")
+    public AuthResponseDto retrieveUser(@PathVariable @Positive Long id){
+        return userService.getUserById(id);
+    }
+}
