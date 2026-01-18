c8ce21d4c95611fd793c546c7d021ea1408ba069
colllins <lekecollins2@gmail.com>
2026-01-17 20:47:18 -0500

    Initial commit

---
+++ src/main/java/com/collins/leftover/LeftoverApplication.java
@@ -0,0 +1,12 @@
+package com.collins.leftover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeftoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeftoverApplication.class, args);
+    }
+}
